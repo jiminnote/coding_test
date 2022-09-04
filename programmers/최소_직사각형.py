e0e2573e4d6af5cc5e9a8db203586d4b5825dd25
jiminnote <trace_of_jimin@naver.com>
2022-09-04 17:23:24 +0900

    Feat: test

---
+++ programmers/최소_직사각형.py
@@ -0,0 +1,7 @@
+def solution(sizes):
+    a,b=0,0
+    for i in sizes:
+        i.sort() #입력받은 모든 명함의 리스트를 [작은 수, 큰 수] 로 정렬
+        a=max(a,i[0]) #size[i][0] 중 가장 큰수
+        b=max(b,i[1]) #size[i][1] 중 가장 큰수
+    return a*b 
